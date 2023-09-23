40dc07d7a48ae5a8560133052800212848093523
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-09-24 01:19:23 +0530

    unsuscribing the observable subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private observableSubscription: Subscription;
   constructor() {}
 
   ngOnInit() {
-    interval(1000).subscribe((value) => {
+    this.observableSubscription = interval(1000).subscribe((value) => {
       console.log(value);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.observableSubscription.unsubscribe();
   }
 }
