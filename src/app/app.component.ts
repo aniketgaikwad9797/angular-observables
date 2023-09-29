ab209149b852ea188c72e9608ad5f2e5266e9dc0
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-09-29 20:15:00 +0530

    unsubscibing the activate subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivateService } from './app-activate.service';
+import { Subscription } from 'rxjs-compat';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   appActivated = false;
+  activateSubscription: Subscription;
   constructor(private appActivateService: ActivateService) {}
 
   ngOnInit() {
-    this.appActivateService.onAppActivate.subscribe(
+    this.activateSubscription = this.appActivateService.onAppActivate.subscribe(
       (isAppActivated: boolean) => {
         this.appActivated = isAppActivated;
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.activateSubscription.unsubscribe();
   }
 }
